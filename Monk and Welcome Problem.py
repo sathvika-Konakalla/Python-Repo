5b08063aba382e0d499ff9e6608e177b4305a2c7
sathvika Konakalla <23A91A6127@aec.edu.in>
2024-10-23 18:58:04 +0530

    Uploading code: Monk and Welcome Problem.py

---
+++ Monk and Welcome Problem.py
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class MonkAndWelcome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] A = new int[N];
+        int[] B = new int[N];
+        for (int i = 0; i < N; i++) {
+            A[i] = sc.nextInt();
+        }
+        for (int i = 0; i < N; i++) {
+            B[i] = sc.nextInt();
+        }
+        int[] C = new int[N];
+        for (int i = 0; i < N; i++) {
+            C[i] = A[i] + B[i];
+        }
+        for (int i = 0; i < N; i++) {
+            System.out.print(C[i] + " ");
+        }
+    }
+}
