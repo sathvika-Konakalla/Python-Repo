011a98a83ddbdbca6a4bfda17267746c7d6c5df8
sathvika Konakalla <23A91A6127@aec.edu.in>
2024-10-27 10:58:45 +0530

    Uploading code: Count no. of words.py

---
+++ Count no. of words.py
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class WordCount {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String inputString = scanner.nextLine();
+        int wordCount = countWords(inputString);
+        System.out.println(wordCount);
+    }
+    public static int countWords(String s) {
+        String[] words = s.trim().split("\\s+");
+        return words.length;
+    }
+}
